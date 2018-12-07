489743e3adce92b51948bd0cd951fbb9209cc29c
Rigoberto <rresendez@rigoresendez.com>
2018-12-07 13:06:09 -0600

    table component

--- src/components/TableResultArrange/index.jsx
+++ src/components/TableResultArrange/index.jsx
@@ -7,8 +7,19 @@ import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
+import IconButton from '@material-ui/core/IconButton'
+import Icon from '../Icon'
 
 class TableResultArrange extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      editable: false
+    }
+  }
+  handleOnClickEditableButton = () => {
+    this.setState({ editable: true })
+  }
   renderRows = () => {
     const rows = this.props.rows.map((row, rowIdx) => {
       const cells = this.props.columns.map((column, columnIdx) => {
@@ -21,20 +32,64 @@ class TableResultArrange extends React.Component {
 
     return rows
   }
-
+  renderButton = (idx) => {
+    if (idx === this.props.columns.length - 1) {
+      return (
+        <IconButton
+          aria-label="save"
+          onClick={this.handleOnClickEditableButton}
+        >
+          <Icon color="black" size="1.5">save</Icon>
+        </IconButton>
+      )
+    }
+    return null
+  }
   renderColumns = () => {
-    const colmuns = this.props.columns.map((column, idx) => (
-      <TableCell key={column.name}>
-        <div draggable="true">
-          {column.alias}
-        </div>
-      </TableCell>
-    ))
-    return colmuns
+    if (this.state.editable) {
+      const colmuns = this.props.columns.map((column, idx) => {
+        return (
+          <TableCell key={column.name}>
+            <div onDragOver={this.dragoverHandler}
+              onDrop={this.dropHandler}
+            >
+              <div draggable="true"
+                data-column={JSON.stringify({ ...column, idx: idx })}
+                onDragStart={this.dragstartHandler}
+              >
+              </div>
+            </div>
+          </TableCell>
+        )
+      })
+      return colmuns
+    } else {
+      const colmuns = this.props.columns.map((column, idx) => (
+        <TableCell key={column.name}>
+          <div onDragOver={this.dragoverHandler}
+            onDrop={this.dropHandler}
+          >
+            <div draggable="true"
+              data-column={JSON.stringify({ ...column, idx: idx })}
+              onDragStart={this.dragstartHandler}
+            >
+              {column.alias}
+            </div>
+            {this.renderButton(idx)}
+          </div>
+        </TableCell>
+      ))
+      return colmuns
+    }
   }
 
   handleClick = () => {
     const columns = [
+      {
+        name: 'item_nbr',
+        alias: 'item_nbr',
+        place: 0
+      },
       {
         name: 'dept_nbr',
         alias: 'depto',
@@ -44,16 +99,31 @@ class TableResultArrange extends React.Component {
         name: 'cid_nbr',
         alias: 'cid value',
         place: 2
-      },
-      {
-        name: 'item_nbr',
-        alias: 'item_nbr',
-        place: 0
       }
     ]
+    const temp = columns[0]
+    columns[0] = columns[2]
+    columns[2] = temp
     this.props.handleColumnsSortChange(columns)
   }
-
+  dragstartHandler = (event) => {
+    event.dataTransfer.setData('application/json', (event.target.getAttribute('data-column')))
+    console.log(event)
+  }
+  dragoverHandler = (event) => {
+    event.preventDefault()
+    console.log('dragging')
+  }
+  dropHandler = (event) => {
+    event.preventDefault()
+    const dropped = JSON.parse(event.target.getAttribute('data-column'))
+    const trans = JSON.parse(event.dataTransfer.getData('application/json'))
+    const columns = this.props.columns
+    const temp = columns[trans.idx]
+    columns[trans.idx] = columns[dropped.idx]
+    columns[dropped.idx] = temp
+    this.props.handleColumnsSortChange(columns)
+  }
   render () {
     return (
       <div>
