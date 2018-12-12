c2a114f554abf4112a40097b1f30629e391a6e2f
sezzh <sezzhltd@gmail.com>
2018-12-12 11:36:53 -0600

    fixed run script

--- run-front.js
+++ run-front.js
@@ -2,7 +2,7 @@ const exec = require('child_process').exec
 const path = require('path')
 const hostname = 'frontend-module.kunkurfrontexampledeployqa.kunkurfrontexampledeploy.bootcamp.dfw1.qa.walmart.com'
 
-exec(`${path.resolve(__dirname, 'node_modules', '.bin', 'serve')} -s build -l tcp://${hostname}5000`, (error, stdout, stderr) => {
+exec(`${path.resolve(__dirname, 'node_modules', '.bin', 'serve')} -s build -l tcp://${hostname}:5000`, (error, stdout, stderr) => {
   if (error) {
     console.error(`exec error: ${error}`)
     return
