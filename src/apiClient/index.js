7da47ec9b4ba280430465483bc37ea66212765d3
sezzh <sezzhltd@gmail.com>
2019-01-17 12:04:25 -0600

    commented token configuration

--- src/apiClient/index.js
+++ src/apiClient/index.js
@@ -4,19 +4,19 @@ export const _dev = process.env.NODE_ENV === 'development'
 
 let BASE_URL = _dev
   ? 'http://localhost:4444/v1/api'
-  : `${window.location.origin}/v1/api`
-
+  : `http://kunkur-global-backend.demo.demo-kunkur-international.bootcamp.qa.walmart.com:4444/v1/api`
+/*
 if (!_dev) {
   if (!localStorage.getItem('token')) {
     window.location.href = `${window.location.origin}/`
   }
 }
-
+*/
 const _axios = axios.create({
   baseURL: BASE_URL,
   timeout: 30000,
   headers: {
-    'token': localStorage.getItem('token')
+    'token': localStorage.getItem('token') || 'holi'
   }
 })
 
