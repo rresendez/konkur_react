c8813adfbfb2bb7983037bcbe827b4141b44d6d5
Samra Ahmed <samra.ahmed0@walmart.com>
2019-01-25 11:12:25 -0500

    Dashboard - List of cards

--- src/components/Dashboard/CardViewList.js
+++ src/components/Dashboard/CardViewList.js
@@ -4,21 +4,7 @@ import CardView from './CardView';
 import { testData } from './store.js'
 
 const CardViewList = () => {
-    console.log(testData);
-    //const data = {
-    //    win: 2345,
-    //        cardId: 1,
-    //        cardComponent: 'INVENTORY',
-     //       cardSubComponent: 'DC Inv',
-     //       cardTask: 'DC Trapped Inv',
-      //      cardLevel: 'Item-DC', 
-      //      taskCount: 2417, 
-      //      updateDate: '2019-01-14',
-      //      colorCode: "#76c043",
-      //      dataLoadDate: '2019-01-14',
-       //     isDelete: 0,
-       //     fileDocument: ''
-        //};
+    
     return(
         <div>
            <Grid container >
